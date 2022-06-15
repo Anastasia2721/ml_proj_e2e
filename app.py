e57e1beb9777b41e28d9b6615e566aa413fadadb
Anastasia2721 <st095434@student.spbu.ru>
2022-06-16 01:32:59 +0300

    Supported 2 types of models. Added E2E Lab1_3.ipynb file.

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ from flask import Flask, request, jsonify
 import joblib
 import numpy
 
-MODEL_PATH = 'model/model.pkl'
+RF_MODEL_PATH = 'model/random_forest_model.pkl'
+CB_MODEL_PATH = 'model/catboost_model.pkl'
 SCALER_X_PATH = 'model/scaler_x.pkl'
 SCALER_Y_PATH = 'model/scaler_y.pkl'
 
 app = Flask(__name__)
-model = joblib.load(MODEL_PATH)
+rf_model = joblib.load(RF_MODEL_PATH)
+cb_model = joblib.load(CB_MODEL_PATH)
 sc_x = joblib.load(SCALER_X_PATH)
 sc_y = joblib.load(SCALER_Y_PATH)
 
@@ -22,7 +24,8 @@ def root_method():
 #   "open_plan": 1,
 #   "rooms": 1,
 #   "area": 2.2,
-#   "renovation": 1
+#   "renovation": 1,
+#   "model": "RF"
 # }
 @app.route('/api/predict', methods=['POST'])
 def predict_post():
@@ -32,10 +35,11 @@ def predict_post():
     rooms = content['rooms']
     area = content['area']
     renovation = content['renovation']
-    return predict(open_plan, rooms, area, renovation)
+    model = content['model']
+    return predict(model, open_plan, rooms, area, renovation)
 
 
-# GET http://localhost:5000?open_plan=1&rooms=4&area=1000.53&renovation=1
+# GET http://localhost:5000?open_plan=1&rooms=4&area=1000.53&renovation=1&model=RF
 @app.route('/api/predict', methods=['GET'])
 def predict_get():
     args = request.args
@@ -44,13 +48,19 @@ def predict_get():
     rooms = args.get('rooms', default=-1, type=int)
     area = args.get('area', default=-1, type=float)
     renovation = args.get('renovation', default=-1, type=int)
-    return predict(open_plan, rooms, area, renovation)
+    model = args.get('model', default='RF', type=str)
+    return predict(model, open_plan, rooms, area, renovation)
 
 
-def predict(open_plan, rooms, area, renovation):
+def predict(model, open_plan, rooms, area, renovation):
     x = numpy.array([open_plan, rooms, area, renovation]).reshape(1, -1)
     x = sc_x.transform(x)
-    result = model.predict(x)
+    if model == 'CB':
+        app.logger.info('predict using catboost: %s ', x)
+        result = cb_model.predict(x)
+    else:
+        app.logger.info('predict using random forest: %s ', x)
+        result = rf_model.predict(x)
     result = sc_y.inverse_transform(result.reshape(1, -1))
     price = {'price': result[0][0]}
     app.logger.info('predicted price: %s ', price)
